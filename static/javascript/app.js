60cfde377dcbd23047807f022715242f0f84252e
Jonathan Hess <info@movingwater.ch>
2015-03-29 18:10:18 +0200

    finetuning

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -1,10 +1,4 @@
-// Jump back to top
-$('.header-control').click(function(){
-    $('html, body').animate({
-        scrollTop: $('body').offset().top
-    });
-    
-});
+
 //$('.affix-top.header-control').click(function(){
 //    $('html, body').animate({
 //        scrollTop: $('#content').offset().top
@@ -12,25 +6,46 @@ $('.header-control').click(function(){
 //    
 //});
 
-// Make Header fixed
+
+// Jump back to top
+$('#header-control').click(function(){
+  if(
+    $('#header-control').hasClass('open')
+  ){
+    $('html, body').animate({
+        scrollTop: $('#header-control').offset().top
+    });
+  } else {
+    $('html, body').animate({
+      scrollTop: $('body').offset().top
+    });
+  }
+
+});
+
+// Make  fixed
 $('header').affix({
   offset: {
     top:500,
   }
 });
 $('header').on('affixed.bs.affix', function(){
-  $("header, #content").addClass('fixed close');
-  $("header, #content").removeClass('open');
+  $("#header-control").addClass('fixed close');
+  $("#header-control").removeClass('open');
   $('.header-control .fa').toggleClass('fa-angle-up fa-angle-down');
 });
 
 $('header').on('affixed-top.bs.affix', function(){
-  $("header, #content").removeClass('fixed close');
+  $("#header-control").addClass('open');
+  $("#header-control").removeClass('fixed close');
   $('.header-control .fa').toggleClass('fa-angle-up fa-angle-down');
 
 });
+
+
 
 //slide away Profile Image and reveal address
 $('.profile-image').click(function () {
   $(this).toggleClass('slide-in slide-out');
 });
+
